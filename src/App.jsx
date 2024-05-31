12abfbccbbc4ce68b311db6f1f247612d126b8a9
nuhmansha <nuhmanshanuhmans@gmail.com>
2024-05-31 15:27:52 +0530

    main files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+
+import './App.css'
+import AppRouter from './Routers/AppRouter'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+     
+      {/* <h1 className="text-4xl font-bold text-blue-600">hai</h1> */}
+
+     
+        <AppRouter/>
+      
+    
+
+
+    </div>
+  )
+}
+
+export default App
