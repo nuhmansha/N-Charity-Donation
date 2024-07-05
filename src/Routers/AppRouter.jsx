f37081474974808d93683e73c6e40fcf5ffa66c8
nuhmansha <nuhmanibnunazar@gmail.com>
2024-07-05 19:34:22 +0530

    jwt router added

--- src/Routers/AppRouter.jsx
+++ src/Routers/AppRouter.jsx
@@ -12,6 +12,7 @@ import EventPage from '../Pages/UserSide/CharityEvent/EventPage';
 import ContactUs from '../Pages/UserSide/ContactUs/ContactUs';
 import SignupOtp from '../Pages/UserSide/SignupOtp';
 import LoginPage from '../Pages/UserSide/LoginPage';
+import PrivateRoute from './PrivateRouter';
 
 function MainContent() {
   const location = useLocation();
@@ -21,7 +22,7 @@ function MainContent() {
     <div>
       {!hideNavbarAndFooter && <Navbar />}
       
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<UserHome />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/otp" element={<SignupOtp />} />
@@ -36,7 +37,49 @@ function MainContent() {
 
 
 
+      </Routes> */}
+       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/otp" element={<SignupOtp />} />
+
+        <Route path="/" element={
+          <PrivateRoute>
+            <UserHome />
+          </PrivateRoute>
+        } />
+        <Route path="/about" element={
+          <PrivateRoute>
+            <About />
+          </PrivateRoute>
+        } />
+        <Route path="/volunteer" element={
+          <PrivateRoute>
+            <VolunteerPage />
+          </PrivateRoute>
+        } />
+        <Route path="/causes" element={
+          <PrivateRoute>
+            <Causes />
+          </PrivateRoute>
+        } />
+        <Route path="/blogs" element={
+          <PrivateRoute>
+            <BlogPage />
+          </PrivateRoute>
+        } />
+        <Route path="/events" element={
+          <PrivateRoute>
+            <EventPage />
+          </PrivateRoute>
+        } />
+        <Route path="/contact" element={
+          <PrivateRoute>
+            <ContactUs />
+          </PrivateRoute>
+        } />
       </Routes>
+
 
       {!hideNavbarAndFooter && <Footer />}
     </div>
